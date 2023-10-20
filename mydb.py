9cd639190b94f27aca3e53343f73ec5772b93fe3
Meng Chong <chong1992@hotmail.com>
2023-10-20 23:34:58 +0800

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,21 @@
+#Install MYSQL on your computer
+#https://dev.mysql.com/downloads/installer/
+#pip install mysql
+#pip install mysql-connector
+#pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password',
+    )
+
+#prepare a cursor object
+cursorObject = dataBase.cursor()
+
+#Create a database
+cursorObject.execute("CREATE DATABASE elderco") #CREATE OR DROP
+
+print("All done!")
